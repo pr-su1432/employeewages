2d4f01359bb6c0563b80757be1b28152c47e431f
prasanna lakshmi <prasannalakshmikathi@gmail.com>
2022-05-17 22:49:11 +0530

    [prasanna]:checkstatus employee is present or not

---
+++ employeewages/employeewages/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace employeewages
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            employeeispresentornot employeeispresentornot = new employeeispresentornot();
+            employeeispresentornot.presentorabsent();
+        }
+    }
+}
